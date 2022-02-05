e66fb8fe5f4e0016cea132897202e54b10304daf
jzavier <jztimm@gmail.com>
2022-02-05 14:17:25 -0500

    Created Portfolio, Coins, and Main comp and rendered to screen

---
+++ components/Coin.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import Image from 'next/image'
+import styled from 'styled-components';
+
+const Coin = ({coin}) => {
+    return (
+        <Wrapper>
+            <div>
+                <div style={{flex: 3}}>
+                    <NameCol>
+                        <CoinIcon>
+                            <Image src={coin.logo} alt={coin.name} />
+                        </CoinIcon>
+                    </NameCol>
+                </div>
+            </div>
+        </Wrapper>
+    )
+};
+
+export default Coin;
+
+const Wrapper = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    align-items: center;
+
+    & > div {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem 2rem;
+    }
+`;
+
+const NameCol = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const CoinIcon = styled.div`
+    width: 1.8rem;
+    margin-right: 1rem;
+`;
+
+const Primary = styled.div`
+    margin-bottom: 0.1rem;
+`;
+
+const Secondary = styled.div`
+    color: #8a919e;
+    font-size: 0.8rem;
+`;
--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'styled-components'
 import Header from '../components/Header'
+import Portfolio from '../components/Portfolio';
 
 const Dashboard = ({address}) => {
     return (
         <Wrapper>
             <MainContainer>
                 <Header />
+                <Portfolio />
             </MainContainer>
         </Wrapper>
     );
