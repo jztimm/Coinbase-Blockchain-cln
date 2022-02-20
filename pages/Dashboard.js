c5222d6dcf6379baaa74cbf3814393f5f7545e23
jzavier <jztimm@gmail.com>
2022-02-20 13:43:31 -0500

    Create and update sidebar comp

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -2,10 +2,12 @@ import React from 'react';
 import styled from 'styled-components'
 import Header from '../components/Header'
 import Portfolio from '../components/Portfolio';
+import Sidebar from '../components/Sidebar';
 
 const Dashboard = ({address}) => {
     return (
         <Wrapper>
+            <Sidebar />
             <MainContainer>
                 <Header />
                 <Portfolio />
