552a864f7ffe825db75d486a3977d905fa087bd5
jzavier <jztimm@gmail.com>
2022-02-21 12:38:12 -0500

    Add Chart and Balance

--- components/Main.js
+++ components/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import Portfolio from './Portfolio';
+import Promos from './Promos';
 
 const Main = () => {
     return (
         <Wrapper>
             <Portfolio />
+            <Promos />
         </Wrapper>
     )
 };
@@ -15,9 +17,9 @@ export default Main;
 const Wrapper = styled.div`
     display: flex;
     max-height: calc(100vh -64px);
-    overflow: hidden;
+    overflow: scroll;
 
     & div {
         border-radius: 0.4rem;
     }
-`;
+`
--- components/Portfolio.js
+++ components/Portfolio.js
@@ -3,11 +3,25 @@ import styled from 'styled-components';
 import {BsThreeDotsVertical} from 'react-icons/Bs';
 import { coins } from '../static/coins'
 import Coin from './Coin';
+import BalanceChart from './BalanceChart';
 
 const Portfolio = () => {
     return (
         <Wrapper>
             <Content>
+                <Chart>
+                    <div>
+                        <Balance>
+                            <BalanceTitle>Portfolio balance</BalanceTitle>
+                            <BalanceValue>
+                                {'$'}
+                                {/* {walletBalance.toLocaleString()} */}
+                                56,000
+                            </BalanceValue>
+                        </Balance>
+                    </div>
+                <BalanceChart />
+                </Chart>
                 <PortfolioTable>
                     <TableItem>
                         <Title>Your Assets</Title>
@@ -54,6 +68,24 @@ const Content = styled.div`
     max-width: 1000px;
     padding: 2rem 1rem;
 `;
+
+const Chart = styled.div`
+    border: 1px solid #282b2f;
+    padding: 1rem 2rem;
+`
+
+const Balance = styled.div``;
+
+const BalanceTitle = styled.div`
+    color: #8a919e;
+    font-size: 0.9rem;
+`
+
+const BalanceValue = styled.div`
+    font-size: 1.8rem;
+    font-weight: 700;
+    margin: 0.5rem 0;
+`
 
 const PortfolioTable = styled.div`
     margin-top: 1rem;
